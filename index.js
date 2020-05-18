79de60b9fe857f01bc9161b27978df2542827765
MuskanSr <muskan.srivastava1904@gmail.com>
2020-05-18 09:33:35 +0530

    Improved UX

--- index.js
+++ index.js
@@ -23,11 +23,38 @@ $(document).ready(function() {
             var currentClass = $(this).attr('class');
             if ($(this).prop('checked')) {
                 $('.' + currentClass).prop('checked', true);
+                //$("#booked").prop('checked',false);
+                //$("#hold").prop('checked',false);
+                //$("#booked1").prop('checked',false);
+                $(".booked").prop('checked',false);
+                $(".hold").prop('checked',false);
             } else {
                 $('.' + currentClass).prop('checked', false);
             }
         }); 
         
-    }); 
+    });
+
+    
+
+    /*$("#check").on("click", function() {
+        $('input').on('change', function () {
+            var currentClass = $(this).attr('class');
+            if ($(this).prop('checked')) {
+                if ($("#booked")) {
+                    console.log("enter");
+                    $('.' + currentClass).prop('checked', false);
+                }
+                else {
+                    console.log("not");
+                    $('.' + currentClass).prop('checked', true);
+                }
+                //$('.' + currentClass).prop('checked', true);
+            } else {
+                $('.' + currentClass).prop('checked', false);
+            }
+        }); 
+        
+    }); */
 })
 
